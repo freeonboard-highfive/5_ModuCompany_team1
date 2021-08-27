6af8e021cc63631330b85d42020e30fbb9b55758
hurima90-kim <hurima90@gmail.com>
2021-08-27 16:12:37 +0900

    style: edit css for width, height in body

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         font-family: 'Noto Sans KR', sans-serif;
+        width: 100vw;
         height: 100vh;
         background-color: #f1f4fc;
     };
